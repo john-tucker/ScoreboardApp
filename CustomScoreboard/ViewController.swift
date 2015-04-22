dcf6f1146ab32abc6ee0dde87cfa127b220c438a
john-tucker <jtucker1996@gmail.com>
2015-04-22 11:17:50 -0500

    4/22/15 Initial commit

---
+++ CustomScoreboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomScoreboard
+//
+//  Created by Tucker on 4/21/15.
+//  Copyright (c) 2015 TuckerSamEmily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
