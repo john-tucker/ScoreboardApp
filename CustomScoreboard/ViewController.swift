a1ab3a482bc873da6b5d9825a50e3cb1fe17f59f
Sam Gadbois <sgadbois@bsd220.org>
2015-04-28 11:46:03 -0500

    workingPicker

--- CustomScoreboard/ViewController.swift
+++ CustomScoreboard/ViewController.swift
@@ -8,14 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    var numberOfPlayers = ["1", "2", "3", "4"]
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return numberOfPlayers.count
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return numberOfPlayers[row]
+    }
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
