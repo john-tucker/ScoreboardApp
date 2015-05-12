52d3595e4ff6c762effa1afc40120577f787721b
Sam Gadbois <sgadbois@bsd220.org>
2015-05-12 11:37:15 -0500

    nightmode

--- CustomScoreboard/SettingsViewController.swift
+++ CustomScoreboard/SettingsViewController.swift
@@ -11,18 +11,37 @@ import UIKit
 class SettingsViewController: UIViewController
 {
 
+    var nightModeOn : Bool = false
+    @IBOutlet weak var nightModeLabel: UILabel!
+    
     @IBAction func nightButton(sender: UIButton)
     {
+        nightModeOn = !nightModeOn
         nightMode()
     }
     
     func nightMode()
     {
-        var night = UIColor.blackColor()
-        ViewController(view.backgroundColor = night)
-        GreedSetupVC(view.backgroundColor = night)
-        ShotsViewController(view.backgroundColor = night)
-        GreedViewController(view.backgroundColor = night)
+        var nightBackGround = UIColor.darkGrayColor()
+        var nightText = UIColor.lightTextColor()
+        var dayBackGround = UIColor.whiteColor()
+        var dayText = UIColor.darkTextColor()
+        if nightModeOn == true
+        {
+            ViewController(view.backgroundColor = nightBackGround)
+            GreedSetupVC(view.backgroundColor = nightBackGround)
+            ShotsViewController(view.backgroundColor = nightBackGround)
+            GreedViewController(view.backgroundColor = nightBackGround)
+            nightModeLabel.textColor = nightText
+        }
+        else
+        {
+            ViewController(view.backgroundColor = dayBackGround)
+            GreedSetupVC(view.backgroundColor = dayBackGround)
+            ShotsViewController(view.backgroundColor = dayBackGround)
+            GreedViewController(view.backgroundColor = dayBackGround)
+            nightModeLabel.textColor = dayText
+        }
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
